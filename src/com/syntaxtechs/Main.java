5640c721b504a41c0965340f59db54d435429fa9
walter fenley <walterfenley@outlook.com>
2020-08-27 01:25:18 +0500

    Test

---
+++ src/com/syntaxtechs/Main.java
@@ -0,0 +1,40 @@
+package com.syntaxtechs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int  g,h,y=12,u=10;
+
+        byte number1;
+        number1 =10;   // size byte is 8 bits
+        short number2=1000; // size byte is 16 bits
+        int number3=1000000000;  // size byte is 32 bits
+        long number4=100000; // size byte is 64 bits
+	// write your code here
+        float a =12.66666666666666666666666666666666f;
+        double d=12125.6666666666666666666666666;
+
+        double sum=a+d;
+        double harderway=12.66666666666666666666666666666666f+12125.6666666666666666666666666;
+        System.out.println(a);
+        System.out.println(d);
+
+        char c='+';
+
+        int numberA1=10;
+        System.out.println(numberA1);
+        numberA1=20;
+        System.out.println(numberA1);
+
+
+
+    }
+
+
+    private class FirstJava {
+
+    }
+
+}
